e7186f66164940ef7f222e188405ffe59b6e7f46
Iago <iagofalcom@hotmail.com>
2018-06-18 21:34:49 -0300

    Att

---
+++ PizzaWEB/src/main/java/com/Pizzaweb/PizzaWEB/controllers/EventoController.java
@@ -0,0 +1,12 @@
+package com.Pizzaweb.PizzaWEB.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class EventoController {
+	@RequestMapping("/CadastrarEvento")
+	public String form() {
+		return"evento/formEvento";
+	}
+}
