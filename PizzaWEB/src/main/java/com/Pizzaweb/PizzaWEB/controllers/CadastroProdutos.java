cb855e492e916694c55ed8e1b4aee84003e709d4
Iago <iagofalcom@hotmail.com>
2018-06-21 20:04:33 -0300

    Update

--- PizzaWEB/src/main/java/com/Pizzaweb/PizzaWEB/controllers/CadastroProdutos.java
+++ PizzaWEB/src/main/java/com/Pizzaweb/PizzaWEB/controllers/CadastroProdutos.java
@@ -4,10 +4,11 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-public class EventoController {
+public class CadastroProdutos {
 	
-	@RequestMapping("/CadastrarEvento")
+	@RequestMapping("/product")
 	public String form() {
-		return"evento/formEvento";
+		return"Produtos/product";
 	}
 }
+
